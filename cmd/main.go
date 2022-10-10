cca3bc06ca96403d5ec1f01d817f9457f15e98c0
135yshr <isago@oreha.dev>
2022-10-10 23:33:56 +0900

    Initial commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "log"
+
+    "github.com/135yshr/go-confernce-mini-2022/server"
+    "github.com/135yshr/go-confernce-mini-2022/handlers"
+)
 
 func main() {
     fmt.Println("Hello, World!")
+    s := server.New()
+    s.AddHandle("/hello", handlers.NewHelloHandler())
+    log.Fatal(s.Start())
 }
