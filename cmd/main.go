e7cf871d5f3af539fc41347a9919cd52a3bd727c
isago <y.isago@secure-cycle.co.jp>
2022-10-15 06:42:32 +0900

    :coffin: del sample code

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,11 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/135yshr/go-confernce-mini-2022/handlers"
 	"github.com/135yshr/go-confernce-mini-2022/server"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
 	s := server.New()
-	s.AddHandle("/hello", handlers.NewHelloHandler())
 	log.Fatal(s.Start())
 }
