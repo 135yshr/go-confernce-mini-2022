3a96d404f24de432fe45b2e52be4fc2bd927d5f8
135yshr <isago@oreha.dev>
2022-10-15 06:10:12 +0900

    利用者情報を登録するAPIのユースケースを作成する (#17)

---
+++ usecase/error.go
@@ -0,0 +1,19 @@
+package usecase
+
+import (
+    "fmt"
+)
+
+type UseCaseError struct {
+    code    int
+    message string
+}
+
+func (e *UseCaseError) Error() string {
+    return fmt.Sprintf("code: %d, message: %s", e.code, e.message)
+}
+
+func NewError(code int, message string) *UseCaseError {
+    return nil
+}
+
